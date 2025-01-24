0c172fdef2b805b83d19bd3ec355bd0f60dafb98
Esataydin <51443456+Esataydin@users.noreply.github.com>
2025-01-25 01:18:22 +0300

    registration form, functionality added

--- meetups/views.py
+++ meetups/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Meetup
 from .forms import RegistrationForm
@@ -15,13 +15,26 @@ def index(request):
 def meetup_details(request, meetup_slug):
     try:
         selected_meetup = Meetup.objects.get(slug=meetup_slug)
-        registration_form = RegistrationForm()
+        if request.method == "GET":
+            registration_form = RegistrationForm()
+            
+        else:
+            registration_form = RegistrationForm(request.POST)
+            if registration_form.is_valid():
+                participant = registration_form.save()
+                selected_meetup.participants.add(participant)
+                return redirect("confirm-registration")
+                
         return render(request, "meetups/meetup-details.html", {
-            "meetup_found": True,
-            "meetup": selected_meetup,
-            "form": registration_form
-        })  
+                "meetup_found": True,
+                "meetup": selected_meetup,
+                "form": registration_form
+            })
+                
     except Exception as exc:
         return render(request, "meetups/meetup-details.html", {
             "meetup_found": False
         })
+        
+def confirm_registration(request):
+    return render(request, "meetups/registration-success.html")
