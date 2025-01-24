634a049fd4c4a979c42a8ff46b0ecbbe7f731bd7
Esataydin <51443456+Esataydin@users.noreply.github.com>
2025-01-25 01:00:03 +0300

    participant form added

--- meetups/views.py
+++ meetups/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from .models import Meetup
+from .forms import RegistrationForm
 
 # Create your views here.
 
@@ -14,10 +15,11 @@ def index(request):
 def meetup_details(request, meetup_slug):
     try:
         selected_meetup = Meetup.objects.get(slug=meetup_slug)
+        registration_form = RegistrationForm()
         return render(request, "meetups/meetup-details.html", {
             "meetup_found": True,
-            "meetup_title": selected_meetup.title,
-            "meetup_description": selected_meetup.description
+            "meetup": selected_meetup,
+            "form": registration_form
         })  
     except Exception as exc:
         return render(request, "meetups/meetup-details.html", {
